8e4224ea08690fcef8d93e2f1179bd5bb9bf7f30
BrunoZimmer <bruno.s.zimmer@hotmail.com>
2020-06-21 17:33:00 -0300

    Atualização Like e Deslike e flatlists

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from "react";
 
 import api from './services/api';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 import {
   SafeAreaView,
   View,
@@ -18,18 +20,28 @@ export default function App() {
   useEffect(() => {
     api.get('repositories').then(response => {
       setRepositories(response.data);
-      //response.data[2].techs.map(tech => {console.log(tech)});
     })
   }, [])
 
   async function handleLikeRepository(id) {
-    const repositoryFind = repositories.find(repository => repository.id === id);
     const response = await api.post(`/repositories/${id}/like`);
 
     const repository = response.data;
     setRepositories([repository, ...repositories.filter((repository) => {return (repository.id !== id)})]);
   }
 
+  async function handleDislikeRepository(id) {
+    const response = await api.post(`/repositories/${id}/dislike`);
+
+    const repository = response.data;
+    setRepositories([repository, ...repositories.filter((repository) => {return (repository.id !== id)})]);
+  }
+
+  async function deleteRepository(id) {
+    const response = await api.delete(`/repositories/${id}`);
+
+    setRepositories([...repositories.filter((repository) => {return (repository.id !== id)})]);
+  }
 
   return (
     <>
@@ -38,39 +50,87 @@ export default function App() {
         <FlatList 
           data = {repositories}
           keyExtractor = {repository => repository.id}
-          style={styles.repositoryContainer}
           renderItem = {({ item: repository }) => (
-            <>
-              <Text style={styles.repository}>
-                {repository.title}
-              </Text>
-
-              <View style={styles.techsContainer}>
-                {repository.techs.map(tech => {
-                  return (<Text style={styles.tech}>{tech}</Text>)
-                })}
+            <View style={styles.repositoryContainer}>
+              
+              <TouchableOpacity
+                style={styles.iconExit}
+                testID={`like-button-${repository.id}`}
+                onPress = {() => deleteRepository(repository.id)}
+              >
+                <Icon name="close" size= {20} />
+              </TouchableOpacity> 
+              
+              <View style={styles.titleContainer}>
+                <Text key={`title-${repository.id}`} style={styles.repository}>
+                  <Icon name="rocket" size= {40} />
+                  {" "}{repository.title}
+                </Text>
               </View>
               
+              <FlatList 
+                style = {styles.flatTech}
+                data = {repository.techs}
+                keyExtractor = {repository => repository.id}
+                horizontal = {true}
+                renderItem = {({ item:tech }) => (
+                  <View style={styles.techsContainer}>
+                    <Text 
+                      key={`repository-tech-${tech}-${repository.id}`} 
+                      style={styles.tech}
+                    >
+                      {tech}
+                    </Text>
+                  </View>
+                )}
+              />
+              
               <View style={styles.likesContainer}>
-                <Text
-                  style={styles.likeText}
-                  testID={`repository-likes-${repository.id}`}
-                >
-                  {repository.likes} curtidas
-                </Text>
+                <View>
+                  <Text
+                    style={styles.likeText}
+                    key={`repository-likes-${repository.id}`}
+                  >
+                    {repository.likes} Likes
+                  </Text>
+                </View>
+                <View>
+                  <Text
+                    style={styles.likeText}
+                    key={`repository-likes-${repository.id}`}
+                  >
+                    {repository.dislikes} Dislikes
+                  </Text>
+                </View>
               </View>
 
-              <TouchableOpacity
-                style={styles.button}
-                onPress={() => handleLikeRepository(repository.id)}
-                testID={`like-button-${repository.id}`}
-              >
-                <Text style={styles.buttonText}>
-                  Curtir
-                </Text>
-              </TouchableOpacity>
 
-            </>
+
+              <View style={styles.likesContainer}>
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => handleLikeRepository(repository.id)}
+                  key={`like-button-${repository.id}`}
+                >
+                  <Text key={`like-text-${repository.id}`} style={styles.buttonText}>
+                    <Icon name="thumbs-o-up" size= {15} />
+                    {"  "}Like
+                  </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => handleDislikeRepository(repository.id)}
+                  key={`dislike-button-${repository.id}`}
+                >
+                  <Text key={`like-text-${repository.id}`} style={styles.buttonText} >
+                    <Icon name="thumbs-o-down" size= {15} borderWidth = {100}/>
+                    {"  "}Dislike
+                  </Text>
+                </TouchableOpacity>
+              </View>
+
+            </View>
           )}
         />
       </SafeAreaView>
@@ -86,16 +146,23 @@ const styles = StyleSheet.create({
   repositoryContainer: {
     marginBottom: 15,
     marginHorizontal: 15,
-    backgroundColor: "#fff",
-    padding: 20,
+    backgroundColor: "#fafafa"
+  },
+  titleContainer: {
+    marginTop: 30,
+    marginHorizontal: 30,
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   repository: {
     fontSize: 32,
     fontWeight: "bold",
   },
   techsContainer: {
-    flexDirection: "row",
-    marginTop: 10,
+    marginTop: 20
+  },
+  flatTech: {
+    marginHorizontal: 10
   },
   tech: {
     fontSize: 12,
@@ -106,25 +173,35 @@ const styles = StyleSheet.create({
     paddingVertical: 5,
     color: "#fff",
   },
+  iconExit: {
+    marginLeft:330,
+    marginTop:15,
+    marginRight:-30,
+    backgroundColor: '#fafafa',
+    position: "absolute"
+  },
   likesContainer: {
-    marginTop: 15,
+    marginTop: 20,
+    margin: 15,
     flexDirection: "row",
     justifyContent: "space-between",
   },
-  likeText: {
-    fontSize: 14,
-    fontWeight: "bold",
-    marginRight: 10,
-  },
   button: {
-    marginTop: 10,
+    flex: 1,
   },
   buttonText: {
     fontSize: 14,
     fontWeight: "bold",
-    marginRight: 10,
+    marginHorizontal: 10,
     color: "#fff",
     backgroundColor: "#7159c1",
-    padding: 15,
+    padding: 12,
+  },
+  likeText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#000",
+    marginHorizontal: 20,
+    marginBottom: -10
   },
 });
